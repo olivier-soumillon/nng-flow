217f8f4b25c8df24fb1fa931ae51b6b784600b48
Olivier Soumillon <olivier.soumillon@gmail.com>
2019-07-22 00:31:05 +0200

    fix alignment column

--- src/components/List.js
+++ src/components/List.js
@@ -4,6 +4,12 @@ const List = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+  :first-of-type {
+    border-right: 1px solid transparent;
+  }
+  :not(:first-of-type) {
+    border-left: 1px solid transparent;
+  }
 `
 
 export default List
